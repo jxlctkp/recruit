7b76866327f6f21922cb421f93f5563b5460b25b
cateleon <yx.leon@qq.com>
2017-08-17 02:12:43 +0800

    update for RE img

--- yuanxin/201708/201708-10-get-img-update.py
+++ yuanxin/201708/201708-10-get-img-update.py
@@ -14,21 +14,14 @@ import re
 # reload(sys)
 # sys.setdefaultencoding('utf-8')
 r = requests.get(url='https://www.baidu.com')
-# print(r.text)
 res = r.text
 
 pattern = r"www.baidu.com.*logo.*(png|jnp)"
-m = re.search(pattern, res )    #  这里是怎么匹配到 logo1 的呢？
-if m:
-    print('01' , m.group(0))
+m = re.search(pattern, res ) 
 
 imgurl = 'http://'+m.group(0)
-print('02',imgurl)
 response = requests.get(imgurl)
-print(response.status_code)
-print('03',imgurl)
 resurl = requests.get(imgurl)
 file = open(imgurl.split('/')[-1],'wb')
-print('05',file)
 file.write(resurl.content)
 file.close()
